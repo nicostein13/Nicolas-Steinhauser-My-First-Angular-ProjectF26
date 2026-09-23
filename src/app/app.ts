ecb5b524ff3b6a14f4e8a5e19960bef6d61569da
Nicolas Steinhauser <w0877732@myscc.ca>
2026-09-23 01:50:52 -0400

    Display data, loops and Bonus

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Pokemon} from './shared/model/pokemon';
+import { Pokemon } from './shared/model/pokemon';
 
 @Component({
   imports: [RouterOutlet],
@@ -9,7 +9,27 @@ import {Pokemon} from './shared/model/pokemon';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Nicolas-Steinhauser-Learning-AngularF26');
+
+  title = 'PokeDex';
+
+  pokemonList: Pokemon[] = [
+    {id: 1, name: 'Charizard', passive: 'Blaze', primaryType: 'Fire', secondaryType: 'Flying', status: 'active'},
+    {id: 2, name: 'Lucario', passive: 'Steadfast or Inner', primaryType: 'Fighting', secondaryType: 'Steel', status: 'active'},
+    {id: 3, name: 'Darkrai', passive: 'Bad Dreams', primaryType: 'Dark', status: 'active'},
+    {id: 4, name: 'Rayquaza', passive: 'Air Lock', primaryType: 'Dragon', secondaryType: 'Flying', status: 'active'},
+    {id: 5, name: 'Cresselia', passive: 'Levitate', primaryType: 'Fairy', status: 'active'},
+    {id: 6, name: 'Kyurem', passive: 'Pressure', primaryType: 'Dragon', secondaryType: 'Ice', status: 'inactive'},
+  ]
 }
+//export class AppComponent{
+ // title = 'PokeDex';
 
-
+ // pokemonList: Pokemon[] = [
+  //  {id: 1, name: 'Charizard', passive: 'Blaze',  primaryType: 'Fire', secondaryType: 'Flying', status: 'active'},
+ //   {id: 2, name: 'Lucario', passive: 'Steadfast or Inner',  primaryType: 'Fighting', secondaryType: 'Steel', status: 'active'},
+//    {id: 3, name: 'Darkrai', passive: 'Bad Dreams',  primaryType: 'Dark', status: 'active'},
+  //  {id: 4, name: 'Rayquaza', passive: 'Air Lock',  primaryType: 'Dragon', secondaryType: 'Flying', status: 'active'},
+ //   {id: 5, name: 'Cresselia', passive: 'Levitate',  primaryType: 'Fairy', status: 'active'},
+ //   {id: 6, name: 'Kyurem', passive: 'Pressure',  primaryType: 'Dragon', secondaryType: 'Ice', status: 'active'},
+ // ]
+//}
